f6db55da00cfc44d481b0ad2368e1372479a93b3
KajalLad1206 <kajallad1994@gmail.com>
2025-05-16 00:02:39 +0530

    initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,13 @@
+
+#!/bin/usr/env groovy
+def call()
+{
+    withCredentials([
+            usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'USER', passwordVariable: 'PWD')
+    ]) {
+        sh 'echo Logging into Docker...'
+        sh 'docker build -t username/java-maven-app:1.4 java-maven-app/'
+        sh 'echo $PWD | docker login -u $USER  --password-stdin'
+        sh 'docker push username/java-maven-app:1.4'
+    }
+}
