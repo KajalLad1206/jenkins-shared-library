0a58d789ea6c2c4d1f046dee2519bb1f4f6d36f5
KajalLad1206 <kajallad1994@gmail.com>
2025-05-16 00:57:23 +0530

    update changes in buildjar file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -6,8 +6,8 @@ def call()
             usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'USER', passwordVariable: 'PWD')
     ]) {
         sh 'echo Logging into Docker...'
-        sh 'docker build -t username/java-maven-app:1.4 java-maven-app/'
+        sh 'docker build -t username/java-maven-app:1.5 .'
         sh 'echo $PWD | docker login -u $USER  --password-stdin'
-        sh 'docker push username/java-maven-app:1.4'
+        sh 'docker push username/java-maven-app:1.5'
     }
 }
