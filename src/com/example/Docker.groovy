22626d133200d4774e3bac5803360586b6d54140
KajalLad1206 <kajallad1994@gmail.com>
2025-05-16 01:57:47 +0530

    update changes in buildimage file

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+
+package com.example
+
+class Docker implements Serializable {
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+    def buildDockerImage(String imageName){
+        script.withCredentials([
+                usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'USER', passwordVariable: 'PWD')
+        ]) {
+            script.sh 'echo Logging into Docker...'
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PWD | docker login -u $script.USER  --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+
+
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,13 +1,15 @@
 #!/bin/usr/env groovy
-
+import com.example.Docker
 def call(String imageName)
 {
-    withCredentials([
-            usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'USER', passwordVariable: 'PWD')
-    ]) {
-        sh 'echo Logging into Docker...'
-        sh "docker build -t $imageName ."
-        sh "echo $PWD | docker login -u $USER  --password-stdin"
-        sh "docker push $imageName"
-    }
+    return new Docker(this).buildDockerImage(imageName)
+
+    //    withCredentials([
+    //            usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'USER', passwordVariable: 'PWD')
+    //    ]) {
+    //        sh 'echo Logging into Docker...'
+    //        sh "docker build -t $imageName ."
+    //        sh "echo $PWD | docker login -u $USER  --password-stdin"
+    //        sh "docker push $imageName"
+    //    }
 }
