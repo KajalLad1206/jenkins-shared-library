7b0a93436aa9be65dc1e0e759b197e8370c95f1b
KajalLad1206 <kajallad1994@gmail.com>
2025-05-16 01:03:50 +0530

    update changes in buildimage file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -7,7 +7,7 @@ def call()
     ]) {
         sh 'echo Logging into Docker...'
         sh 'docker build -t kajallad126/java-maven-app:1.5 .'
-        sh 'echo $PWD | docker login -u $USER  --password-stdin'
+        sh "echo $PWD | docker login -u $USER  --password-stdin"
         sh 'docker push kajallad126/java-maven-app:1.5'
     }
 }
